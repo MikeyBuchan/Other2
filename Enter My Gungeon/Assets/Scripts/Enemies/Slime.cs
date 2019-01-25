98476f6b6960346424192c50d531d5ab7c65a007
MikeyBuchan <psmbgames@hotmail.com>
2019-01-25 22:11:54 +0100

    asd

---
+++ Enter My Gungeon/Assets/Scripts/Enemies/Slime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : Enemy_Main 
+{
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+}
